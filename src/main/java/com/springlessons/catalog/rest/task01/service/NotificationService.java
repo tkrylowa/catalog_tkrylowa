b04b7339bbf9dbe944f3b7abf5f588c67e89d1df
tkrylowa <krylovatanusha@gmail.com>
2024-07-14 15:42:14 +0300

    Задача: 07.11.rest.task-02 by Tatiana Krylova: add Service annotation

--- src/main/java/com/springlessons/catalog/rest/task01/service/NotificationService.java
+++ src/main/java/com/springlessons/catalog/rest/task01/service/NotificationService.java
@@ -4,9 +4,11 @@ import com.springlessons.catalog.rest.task01.executor.CityRestExecutor;
 import com.springlessons.catalog.rest.task01.repository.CityRepository;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
 
+@Service
 public class NotificationService {
     private final CityRepository cityRepository;
     private final CityRestExecutor cityRestExecutor;
