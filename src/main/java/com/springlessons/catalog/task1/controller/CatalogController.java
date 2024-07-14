ffba208cd1f7fc0c3a589c20c6769577e4be2f1b
tkrylowa <krylovatanusha@gmail.com>
2024-07-14 15:09:05 +0300

    Задача: 07.11.rest.task-01 by Tatiana Krylova: Controller -> RestController

--- src/main/java/com/springlessons/catalog/task1/controller/CatalogController.java
+++ src/main/java/com/springlessons/catalog/task1/controller/CatalogController.java
@@ -2,24 +2,24 @@ package com.springlessons.catalog.task1.controller;
 
 import com.springlessons.catalog.task1.dto.TraderDto;
 import com.springlessons.catalog.task1.service.TradersService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/api/v1/catalog")
 public class CatalogController {
-    TradersService tradersService;
+    private final TradersService tradersService;
 
     public CatalogController(TradersService tradersService) {
         this.tradersService = tradersService;
     }
 
     @GetMapping("/traders")
-    public List<TraderDto> getTradersByCityIds(@RequestHeader("x-city-id") int cityId) {
+    public List<TraderDto> getTradersByCityId(@RequestHeader("x-city-id") int cityId) {
         return tradersService.getTradersByCityId(cityId);
     }
 }
