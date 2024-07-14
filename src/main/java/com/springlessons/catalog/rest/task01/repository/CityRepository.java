b90bc50f4c1f997915c73787f533d894df915fac
tkrylowa <krylovatanusha@gmail.com>
2024-07-14 15:13:19 +0300

    Задача: 07.11.rest.task-01 by Tatiana Krylova: add Repository

--- src/main/java/com/springlessons/catalog/rest/task01/repository/CityRepository.java
+++ src/main/java/com/springlessons/catalog/rest/task01/repository/CityRepository.java
@@ -2,6 +2,8 @@ package com.springlessons.catalog.rest.task01.repository;
 
 import com.springlessons.catalog.rest.task01.entity.City;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CityRepository extends JpaRepository<City, Long> {
 }
