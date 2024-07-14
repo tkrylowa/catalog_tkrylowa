cebed11a3c8f2eb0322120a68b1ea906ac6f00e2
tkrylowa <krylovatanusha@gmail.com>
2024-07-14 15:06:56 +0300

    Задача: 07.11.rest.task-01 by Tatiana Krylova: create necessary classes for task

---
+++ src/main/java/com/springlessons/catalog/rest/task01/service/NotificationService.java
@@ -0,0 +1,24 @@
+package com.springlessons.catalog.rest.task01.service;
+
+import com.springlessons.catalog.rest.task01.executor.CityRestExecutor;
+import com.springlessons.catalog.rest.task01.repository.CityRepository;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.concurrent.TimeUnit;
+
+public class NotificationService {
+    private final CityRepository cityRepository;
+    private final CityRestExecutor cityRestExecutor;
+
+    public NotificationService(CityRepository cityRepository, CityRestExecutor cityRestExecutor) {
+        this.cityRepository = cityRepository;
+        this.cityRestExecutor = cityRestExecutor;
+    }
+
+    @Async("city-executor")
+    @Scheduled(fixedRate = 24, timeUnit = TimeUnit.HOURS)
+    public void updateCityInformation() {
+        cityRepository.saveAll(cityRestExecutor.restExecutor());
+    }
+}
