21f8b8e09075c465f6d9a7e4430c2fd68eb15066
tkrylowa <krylovatanusha@gmail.com>
2024-07-14 15:43:50 +0300

    Задача: 07.11.rest.task-01 by Tatiana Krylova: add RestConfiguration

---
+++ src/main/java/com/springlessons/catalog/rest/RestConfiguration.java
@@ -0,0 +1,18 @@
+package com.springlessons.catalog.rest;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
+
+@Configuration
+public class RestConfiguration {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplateBuilder()
+                .setConnectTimeout(Duration.ofMillis(5000))
+                .build();
+    }
+}
